44bba20f606903ebd39d345f413fee158d4117aa
Jack Chan <jack.chan@flukenetworks.com>
2016-07-19 15:18:28 -0700

    WIP, haven't added click event to select hero

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,49 @@
 import { Component } from '@angular/core';
 
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
+
+
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template:`
+        <h1>{{title}}</h1>
+        <h2>My Heroes</h2>
+        <ul class="heroes">
+            <li *ngFor="let hero of heroes">
+                <span class="badge">{{hero.id}}</span> {{hero.name}}
+            </li>
+        </ul>
+        <h2>
+        {{hero.name}} Details</h2>
+        <div><label>id: {{hero.id}}</label></div>
+        <div>
+            <label>name: </label>
+            <input [(ngModel)]="hero.name" placeholder="name">
+        </div>
+        `
 })
-export class AppComponent { }
+export class AppComponent {
+    public heroes = HEROES;
+    title = 'Tour of Heroes';
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
+
+export class Hero {
+    id: number;
+    name: string;
+}
